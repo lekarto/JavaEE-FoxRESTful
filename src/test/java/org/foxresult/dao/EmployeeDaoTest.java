96ac670a75439e6d3623eef91d4c2c657826f34f
lekarto <dpi.ukraine@gmail.com>
2015-04-12 17:01:31 +0300

    Added tests to EmployeeDAO, part 2

--- src/test/java/org/foxresult/dao/EmployeeDaoTest.java
+++ src/test/java/org/foxresult/dao/EmployeeDaoTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class EmployeeDaoTest {
     private String FIRST_NAME_TEST = "Abdul";
     private String LAST_NAME_TEST  = "Ivanov";
+    
     @Autowired
     EmployeeDao employeeDao;
 
@@ -27,7 +28,7 @@ public class EmployeeDaoTest {
     }
 
     @Test
-    public void persistAndGetDepartmentByNameTest() {
+    public void persistAndGetFilteredEmployeesTest() {
         Employee emp = new Employee(FIRST_NAME_TEST, LAST_NAME_TEST, Employee.MALE, 123456789f);
         Assert.assertTrue(employeeDao.persist(emp));
 
@@ -41,8 +42,40 @@ public class EmployeeDaoTest {
         Employee emp_get = list.get(0);
 
         Assert.assertTrue((emp_get.getSex() == emp.getSex()) &&
-                          emp_get.getFirstName().equals(emp.getFirstName()) &&
-                          emp_get.getLastName().equals(emp.getLastName()) &&
-                        (emp_get.getSalary().equals(emp.getSalary())) );
+                emp_get.getFirstName().equals(emp.getFirstName()) &&
+                emp_get.getLastName().equals(emp.getLastName()) &&
+                (emp_get.getSalary().equals(emp.getSalary())));
+    }
+
+    @Test
+    public void persistUpdateDeleteTest() {
+        Employee emp = new Employee(FIRST_NAME_TEST, LAST_NAME_TEST, Employee.MALE, 987654321f);
+        Assert.assertTrue(employeeDao.persist(emp));
+
+        String newName = "Changed";
+        emp.setFirstName(newName);
+        employeeDao.update(emp);
+
+        Employee emp_updated = employeeDao.getByPK(emp.getId());
+        Assert.assertTrue((emp_updated != null) && emp_updated.getFirstName().equals(emp.getFirstName()));
+
+        Assert.assertTrue(employeeDao.deleteByPK(emp_updated.getId()));
+        Assert.assertNull(employeeDao.getByPK(emp_updated.getId()));
+    }
+
+    @Test
+    public void getByPKTest() {
+        Employee emp = employeeDao.getByPK(1);
+        Assert.assertNotNull(emp);
+        Assert.assertTrue(emp.getFirstName().equals("Sergey") &&
+                emp.getLastName().equals("Fedorov"));
+    }
+
+    @Test
+    public void getAllTest() {
+        List emps = employeeDao.getAll();
+        Assert.assertNotNull(emps);
+        int count = emps.size();
+        Assert.assertTrue(count > 0);
     }
 }
